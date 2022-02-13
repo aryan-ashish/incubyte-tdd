32b77d447e8bb55c34336f48e3dcc9b51452fce0
aryan-ashish <aashish.ae1d@gmail.com>
2022-02-13 15:52:15 +0530

    Added test case for two arguments input as string

--- tests/test_add.py
+++ tests/test_add.py
@@ -9,6 +9,9 @@ class MyTestCase(unittest.TestCase):
     def test_add_single_arg(self):
         self.assertEqual(add("99"), 99)
 
+    def test_add_two_args(self):
+        self.assertEqual(add("1,2"), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
