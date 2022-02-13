593e2736a9e13c1aae91e46d515efc39134c73e0
aryan-ashish <aashish.ae1d@gmail.com>
2022-02-13 18:46:44 +0530

    Handled optional delimiter functionality in add.py

--- stringcalculator/add.py
+++ stringcalculator/add.py
@@ -5,12 +5,27 @@ def add(numberStr):
     if numberStr == "":
         return 0
     else:
-        numList = re.split(r'[\\n,]', numberStr)
         sumNum = 0
-        for num in numList:
-            if num != '':
-                sumNum += int(num)
+        if re.match(r'^//', numberStr):
+            charList = re.split(r'\\n', numberStr)
+            delimiter = charList[0][-1]
+
+            numList = re.split(delimiter, charList[1])
+            sumNum = sum_calculator(numList)
+
+        else:
+            numList = re.split(r'[\\n,]', numberStr)
+            sumNum = sum_calculator(numList)
+
         return sumNum
+
+
+def sum_calculator(numberList):
+    resultSum = 0
+    for num in numberList:
+        if num != '':
+            resultSum += int(num)
+    return resultSum
 
 
 if __name__ == "__main__":
