b6fd763eac14853a1ef3c96821ada1c11f87ff70
aryan-ashish <aashish.ae1d@gmail.com>
2022-02-13 19:38:38 +0530

    Added inline documentation to add.py

--- stringcalculator/add.py
+++ stringcalculator/add.py
@@ -1,12 +1,15 @@
 import re
 
 
+# Method to calculate sum of numbers in the string taken as input
+# Handles cases with empty string, with negative arguments, and with optional delimiter change
 def add(numberStr):
     sumNum = 0
 
     if numberStr == "":
         return sumNum
     else:
+        # Handling optional delimiter case by matching '//' at the start
         if re.match(r'^//', numberStr):
             charList = re.split(r'\\n', numberStr)
             if len(charList) > 0:
@@ -14,6 +17,7 @@ def add(numberStr):
                 numList = re.split(delimiter, charList[1])
                 sumNum = sum_calculator(numList)
 
+        # Handling case without optional delimiter change
         else:
             numList = re.split(r'[\\n,]', numberStr)
             sumNum = sum_calculator(numList)
@@ -21,6 +25,8 @@ def add(numberStr):
         return sumNum
 
 
+# Method to calculate some of numbers in the string,
+# or raise exception for negative arguments
 def sum_calculator(numberList):
     resultSum = 0
     negFlag = False
@@ -34,6 +40,8 @@ def sum_calculator(numberList):
             negList.append(int(num))
             negFlag = True
 
+    # if one or more numbers are negative, a ValueError exception is raised
+    # along with a list of negative arguments
     if negFlag is True:
         raise ValueError("negatives not allowed " + str(negList))
     else:
